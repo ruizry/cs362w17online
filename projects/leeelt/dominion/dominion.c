3bda63ba237ada950f4f5892d007c55ad361f366
Elton Lee <eltonkwoklee@gmail.com>
2017-01-21 09:48:13 -0500

    Remove unused variables in dominion.c

--- projects/leeelt/dominion/dominion.c
+++ projects/leeelt/dominion/dominion.c
@@ -655,8 +655,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
-  int cardDrawn;
-  int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
   }
@@ -667,9 +665,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     {
     case adventurer:
   {
-    if (adventurerCard(currentPlayer, state)){
-      return -1;
-    }
+    playAdventurer(currentPlayer, state);
   }
       return 0;
 			
@@ -1315,11 +1311,13 @@ int updateCoins(int player, struct gameState *state, int bonus)
 }
 
 
-int adventurerCard(int currentPlayer, gameState* state)
+int playAdventurer(int currentPlayer, struct gameState *state)
 {
   //initialize local states
   int drawntreasure = 0;  //holds the number of cards drawn from adventurer
   int temphand[MAX_HAND];  //holds the cards drawn from adventurer
+  int z = 0; //counter for temp hand
+  int cardDrawn;
 
   while(drawntreasure<2){
     if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
