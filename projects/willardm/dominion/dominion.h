389f723a4edef47adeee873e7ea8d3dff51bf9cb
michealwillard <willardm@onid.oregonstate.edu>
2017-01-21 12:45:07 -0800

    refactored playAdventurer() and playSmithy() functions

--- projects/willardm/dominion/dominion.h
+++ projects/willardm/dominion/dominion.h
@@ -131,7 +131,7 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
    1, others to 0 */
 
 int playAdventurer(struct gameState *state);
-int playSmithy(struct gameState *state, int handPos);
+int playSmithy(struct gameState *state, int currentPlayer, int handPos);
 int playVillage(struct gameState *state, int handPos);
 int playFeast(struct gameState *state, int choice1);
 int playCouncil_Room(struct gameState *state, int handPos);
