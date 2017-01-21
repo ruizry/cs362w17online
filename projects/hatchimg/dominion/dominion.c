05237477974bbf45de2a978702a3bb2f710d5b5d
hatchimg <hatchimg@oregonstate.edu>
2017-01-21 16:11:10 -0500

    adventurer implemented in own function

--- projects/hatchimg/dominion/dominion.c
+++ projects/hatchimg/dominion/dominion.c
@@ -667,7 +667,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-    	adventurer();
+    	adventurer(state);
           return 0;
 			
     case council_room:
@@ -1311,7 +1311,22 @@ int updateCoins(int player, struct gameState *state, int bonus)
   return 0;
 }
 
-int adventurer(){
+int adventurer(struct gamestate* state){
+
+  int index;
+  int currentPlayer = whoseTurn(state);
+  int nextPlayer = currentPlayer + 1;
+
+  int tributeRevealedCards[2] = {-1, -1};
+  int temphand[MAX_HAND];// moved above the if statement
+  int drawntreasure=0;
+  int cardDrawn;
+  int z = 0;// this is the counter for the temp hand
+  if (nextPlayer > (state->numPlayers - 1)){
+    nextPlayer = 0;
+
+
+
   while(drawntreasure<2){
 	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
 	  shuffle(currentPlayer, state);
