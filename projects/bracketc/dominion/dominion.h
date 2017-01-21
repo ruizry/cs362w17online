ded2e555072518281411a1694047303922e5bd74
chris-brackett <bracketc@oregonstate.edu>
2017-01-21 15:43:49 -0800

    Refactor cards, add bugs
    Refactored smithy, adventurer, village, great hall and minion cards and
    added bugs to each new function

--- projects/bracketc/dominion/dominion.h
+++ projects/bracketc/dominion/dominion.h
@@ -130,11 +130,13 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
-int playAdventurer(struct gameState *state);
-int playSmithy(struct gameState *state, int handPos);
-int playVillage(struct gameState *state, int handPos);
-int playFeast(struct gameState *state, int choice1);
-int playCouncil_Room(struct gameState *state, int handPos);
+int playAdventurer(int currentPlayer, struct gameState *state);
+int playSmithy(int currentPlayer, struct gameState *state, int handPos);
+int playVillage(int currentPlayer, struct gameState *state, int handPos);
+int playGreat_Hall(int currentPlayer, struct gameState *state, int handPos);
+int playMinion(int currentPlayer, struct gameState *state, int handPos, int choice1, int choice2);
+//int playFeast(struct gameState *state, int choice1);
+//int playCouncil_Room(struct gameState *state, int handPos);
 
 
 #endif
